e08e7466924c441e95357f99cea2c43b43caa1f8
Tanvi Agarwal <tanviagarwal@crc-dot1x-nat-10-239-72-11.bu.edu>
2025-11-19 02:18:55 -0500

    final change with checkers

--- lib/getAliasChecker.ts
+++ lib/getAliasChecker.ts
@@ -3,7 +3,11 @@ import getCollection  from "@/db";
 
 export default async function getAliasChecker(alias: string) : Promise<boolean> {
     const links = await getCollection();
+    if (encodeURIComponent(alias) !== alias) {
+        return false;
+    }
     const data = await links.findOne({alias});
+    
     if (!data){
         return true;
     }else{
--- lib/getUrlChecker.ts
+++ lib/getUrlChecker.ts
@@ -1,9 +1,27 @@
+export default async function getUrlChecker(url: string): Promise<boolean> {
+  if (!url) return false;
 
-export default function getUrlChecker(url: string) {
   try {
     new URL(url);
-    return true;
-  } catch (error) {
+  } catch {
     return false;
   }
+
+  if (
+    url.startsWith("https://cs391-url-shortener.vercel.app") ||
+    url.startsWith("http://localhost:3000")
+  ) {
+    return false;
+  }
+
+  try {
+    const res = await fetch(url);
+    if (res.status < 200 || res.status >= 500) {
+      return false;
+    }
+  } catch {
+    return false;
+  }
+
+  return true;
 }
